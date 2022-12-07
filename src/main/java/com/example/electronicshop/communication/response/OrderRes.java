fd5793c6d867370aa4cff41dde115bb511d33ef5
transang832001 <transang8320001@gmail.com>
2022-12-07 21:43:10 +0700

    response thêm createdate and lastmodifieddate

--- src/main/java/com/example/electronicshop/communication/response/OrderRes.java
+++ src/main/java/com/example/electronicshop/communication/response/OrderRes.java
@@ -1,11 +1,15 @@
 package com.example.electronicshop.communication.response;
 
 import com.example.electronicshop.models.enity.Delivery;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -26,14 +30,22 @@ public class OrderRes {
     @JsonInclude(JsonInclude.Include.NON_EMPTY)
     private Delivery delivery;
     private String state;
+    @CreatedDate
+    @JsonFormat(pattern = "dd/MM/yyyy HH:mm:ss")
+    LocalDateTime createdDate;
+    @JsonFormat(pattern = "dd/MM/yyyy HH:mm:ss")
+    @LastModifiedDate
+    LocalDateTime lastModifiedDate;
 
-    public OrderRes(String id, String userId, String userName, long totalProduct, BigDecimal totalPrice, String state) {
+    public OrderRes(String id, String userId, String userName, long totalProduct, BigDecimal totalPrice, String state, LocalDateTime createdDate, LocalDateTime lastModifiedDate) {
         this.id = id;
         this.userId = userId;
         this.userName = userName;
         this.totalProduct = totalProduct;
         this.totalPrice = totalPrice;
         this.state = state;
+        this.createdDate = createdDate;
+        this.lastModifiedDate = lastModifiedDate;
     }
 
     public OrderRes(String id, String userId, String userName, String email, String phone, long totalProduct, BigDecimal totalPrice, String state) {
@@ -45,5 +57,18 @@ public class OrderRes {
         this.totalProduct = totalProduct;
         this.totalPrice = totalPrice;
         this.state = state;
+    }
+
+    public OrderRes(String id, String userId, String userName, String email, String phone, long totalProduct, BigDecimal totalPrice, String state, LocalDateTime createdDate, LocalDateTime lastModifiedDate) {
+        this.id = id;
+        this.userId = userId;
+        this.userName = userName;
+        this.email = email;
+        this.phone = phone;
+        this.totalProduct = totalProduct;
+        this.totalPrice = totalPrice;
+        this.state = state;
+        this.createdDate = createdDate;
+        this.lastModifiedDate = lastModifiedDate;
     }
 }
