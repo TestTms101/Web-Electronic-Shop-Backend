d6905acbacadf62f8ea968d302be6acbf78d6351
transang832001 <transang8320001@gmail.com>
2023-05-10 14:20:43 +0700

    fix stats

--- src/main/java/com/example/electronicshop/repository/OrderRepository.java
+++ src/main/java/com/example/electronicshop/repository/OrderRepository.java
@@ -7,6 +7,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.Aggregation;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -22,6 +23,8 @@ public interface OrderRepository extends MongoRepository<Order, String> {
     List<StateCountAggregate> countAllByState();
     Page<Order> findAllByCreatedDateBetweenAndState(LocalDateTime from, LocalDateTime to, String state, Pageable pageable);
     List<Order> getOrderByUser_IdAndState(ObjectId userId, String state);
-//    Page<Order> 1findAllByCreatedDateBetweenAndState(LocalDateTime from, LocalDateTime to, String state, Pageable pageable);
+    @Query(value=" {state: {'$nin': ['enable']}}")
+    Page<Order> findAllByStateNoEnable( Pageable pageable);
+    Page<Order> countAllByCreatedDateBetweenAndState(LocalDateTime from, LocalDateTime to, String state, Pageable pageable);
 
 }
